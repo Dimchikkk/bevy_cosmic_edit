6ce77f8d201f8b8b75c8b559d441c80c81f9fda3
StaffEngineer <velo.app1@gmail.com>
2023-09-17 12:00:37 +0100

    cargo fmt

--- examples/image_background.rs
+++ examples/image_background.rs
@@ -1,7 +1,7 @@
 use bevy::prelude::*;
 use bevy_cosmic_edit::{
-    bevy_color_to_cosmic, change_active_editor_sprite, change_active_editor_ui,
-    Attrs, AttrsOwned, CosmicAttrs, CosmicBackground, CosmicEditPlugin, CosmicEditUiBundle, Focus,
+    bevy_color_to_cosmic, change_active_editor_sprite, change_active_editor_ui, Attrs, AttrsOwned,
+    CosmicAttrs, CosmicBackground, CosmicEditPlugin, CosmicEditUiBundle, Focus,
 };
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
